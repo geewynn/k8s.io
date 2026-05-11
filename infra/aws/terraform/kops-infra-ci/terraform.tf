81c97eca6f440d33b02925c8a959eb7c527b8efb
Arnaud Meukam <ameukam@gmail.com>
2026-05-11 21:46:28 +0200

    AWS: bump module and provider versions for kOps CI
    - eks/aws: 21.3.2 (exact pin) -> ~> 21.20
    - iam/aws IRSA (3x): ~> 6.2.1 -> ~> 6.6
    - vpc/aws: ~> 6.4.0 -> ~> 6.6
    - hashicorp/aws provider: ~> 6.37.0 -> ~> 6.44
    
    Signed-off-by: Arnaud Meukam <ameukam@gmail.com>

--- infra/aws/terraform/kops-infra-ci/terraform.tf
+++ infra/aws/terraform/kops-infra-ci/terraform.tf
@@ -24,7 +24,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.37.0"
+      version = "~> 6.44"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
